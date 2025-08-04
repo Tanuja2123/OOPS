31893057354a7a27d6a0c2bb031cb409e63ae2dd
Tanuja Sharma <tanujas605@gmail.com>
2025-08-04 18:29:50 +0530

    class

---
+++ Class.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+class student{
+    private:
+    string name;
+    int age, roll_number;
+    string grade;
+    
+    public:
+    void setName(string n) { // can add more functionaliy to check for valid names
+        if(n.size()==0){
+            cout<<"invalid name";
+            return ;
+        }
+        name = n;
+    }
+    void setage(int a){
+        if(age<0 || age>100){
+            cout<<"invalid age";
+            return ;
+        }
+        age = a;
+    }
+    void setRollNumber(int r) {
+        roll_number = r;
+    }
+    void setgrade(string s){
+        grade = s;
+    }
+    void getname() {
+        cout << "Name: " << name << endl;
+    }
+    void getage() {
+        cout << "Age: " << age << endl;
+    }
+    void getRollNumber() {
+        cout << "Roll Number: " << roll_number << endl;
+    }
+    string getgrade( int pin) {
+        if(pin == 123)
+        return grade;
+        else {
+            cout << "Invalid pin" << endl;
+            return "";
+        }
+    }
+};
+int main(){
+
+    // student s1;
+    // s1.name = "rohit";
+    // s1.age= 10;
+    // s1.roll_number=21;
+    // s1.grade= "A+";
+    // cout << "Name: " << s1.name << endl;
+    student s1;
+    s1.setName("rohit");
+    s1.setage(10);
+    s1.setRollNumber(21);
+    s1.setgrade("A+");
+    s1.getname();
+    s1.getage();
+    s1.getRollNumber();
+    s1.getgrade(234); // wrong pin
+    return 0;
+
+}
